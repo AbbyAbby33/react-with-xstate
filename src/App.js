470c71baaa57a494378499a5aea897c5be74025c
Abby <sk573129@gmail.com>
2022-06-13 17:35:17 +0800

    feat: 新增tab切換狀態機

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useMachine } from '@xstate/react';
+import { createMachine } from 'xstate';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const navFocusMachine = createMachine({
+		initial: '0',
+		states: {
+			'0': {
+				on: {
+					COMPLETED: '1',
+					EXPIRED: '2',
+				},
+			},
+			'1': {
+				on: {
+					EXECUTION: '0',
+					EXPIRED: '2',
+				},
+			},
+			'2': {
+				on: {
+					EXECUTION: '0',
+					COMPLETED: '1',
+				},
+			},
+		},
+	});
+	// EXECUTION: 'EXECUTION',
+	// COMPLETED: 'COMPLETED',
+	// EXPIRED: 'EXPIRED',
+	const [state, send] = useMachine(navFocusMachine);
+
+	console.log(state); // 'green'
+
+
+	return (
+		<div>
+			{/** 所處狀態 */}
+			{state.matches('0') && <p>execution</p>}
+			{state.matches('1') && <p>completed</p>}
+			{state.matches('2') && <p>expired</p>}
+			{state.matches('3') && <p>初始狀態</p>}
+			<div>
+				{/** 更改狀態 */}
+				<button onClick={() => send('EXECUTION')}>執行中</button>
+				<button onClick={() => send('COMPLETED')}>已完成</button>
+				<button onClick={() => send('EXPIRED')}>已過期</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
