8554fe635f9dd683525372a6d36690dbd18e53fa
Abby <sk573129@gmail.com>
2022-06-14 11:59:27 +0800

    feat: 1.取得tab目前狀態 2.狀態機加id

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { createMachine } from 'xstate';
 function App() {
 
 	const navFocusMachine = createMachine({
+		id: 'navFocus',
 		initial: '0',
 		states: {
 			'0': {
@@ -29,26 +30,23 @@ function App() {
 			},
 		},
 	});
-	// EXECUTION: 'EXECUTION',
-	// COMPLETED: 'COMPLETED',
-	// EXPIRED: 'EXPIRED',
+
 	const [state, send] = useMachine(navFocusMachine);
 
 	console.log(state); // 'green'
 
-
 	return (
 		<div>
 			{/** 所處狀態 */}
-			{state.matches('0') && <p>execution</p>}
-			{state.matches('1') && <p>completed</p>}
-			{state.matches('2') && <p>expired</p>}
-			{state.matches('3') && <p>初始狀態</p>}
+			{state.matches('0') && <p>執行中</p>}
+			{state.matches('1') && <p>已完成</p>}
+			{state.matches('2') && <p>已過期</p>}
 			<div>
 				{/** 更改狀態 */}
 				<button onClick={() => send('EXECUTION')}>執行中</button>
 				<button onClick={() => send('COMPLETED')}>已完成</button>
 				<button onClick={() => send('EXPIRED')}>已過期</button>
+				<button onClick={() => {console.log('狀態機目前狀態--state.value', state.value)}}>狀態機目前狀態</button>
 			</div>
 		</div>
 	);
